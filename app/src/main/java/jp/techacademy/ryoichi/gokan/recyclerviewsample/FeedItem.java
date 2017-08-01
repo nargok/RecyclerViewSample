8895198e9b29c0c571623090c8331cb7d82d0d8f
nargok <nargok@gmail.com>
2017-08-01 09:03:15 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ryoichi/gokan/recyclerviewsample/FeedItem.java
@@ -0,0 +1,30 @@
+package jp.techacademy.ryoichi.gokan.recyclerviewsample;
+
+/**
+ * Created by houxianliangyi on 2017/08/01.
+ */
+
+public class FeedItem {
+    private String title;
+    private String thumbnail;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+
+
+
+}
